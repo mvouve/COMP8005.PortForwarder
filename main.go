a18c6f717164b4d386266bdb295a51662344305e
Marc Vouve <mvouve@gmail.com>
2016-03-07 05:20:44 -0800

    working feature set

---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+)
+
+type config struct {
+	Forwarder []forward
+	Balencer  []balence
+}
+
+type forward struct {
+	Remote   string
+	Local    string
+	Protocol string
+}
+
+type balence struct {
+	Remote   []string
+	Local    string
+	Protocol string
+}
+
+func main() {
+	conf := loadConfigFile()
+	for _, element := range conf.Balencer {
+		element.run()
+	}
+	for _, element := range conf.Forwarder {
+		element.run()
+	}
+}
+
+func loadConfigFile() config {
+	var conf config
+
+	j, err := ioutil.ReadFile(os.Args[1])
+	perror(err, "error reading config")
+	json.Unmarshal(j, &conf)
+
+	return conf
+}
+
+func (f forward) run() {
+	listen, err := net.Listen(f.Protocol, f.Local)
+	perror(err, "Failed to listen on port "+f.Local)
+	for {
+		dst, _ := net.Dial(f.Protocol, f.Remote)
+		src, _ := listen.Accept()
+		go copy(dst, src)
+		go copy(src, dst)
+	}
+}
+
+func (b balence) run() {
+	listen, err := net.Listen(b.Protocol, b.Local)
+	perror(err, "Failed to listen on port "+b.Local)
+	for i := 0; ; i++ {
+		local, err := listen.Accept()
+		perror(err, "failed to accept")
+		remote, err := net.Dial(b.Protocol, b.Remote[i%len(b.Remote)])
+		perror(err, "could not connect to host "+b.Remote[i%len(b.Remote)])
+		go copy(remote, local)
+		go copy(local, remote)
+	}
+}
+
+func copy(src net.Conn, dst net.Conn) {
+	defer src.Close()
+	defer dst.Close()
+	io.Copy(src, dst)
+}
+
+func perror(err error, str string) {
+	if err != nil {
+		log.Println(str+":", err)
+	}
+}
